57578d1327635fea2ed861d2465d1a1ce16600c0
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-08 20:19:20 +0300

    keymaps

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,3 +1,5 @@
+require('user.term')
+
 local nnoremap = require("user.keymap").nnoremap
 local vnoremap = require("user.keymap").vnoremap
 
@@ -15,11 +17,78 @@ nnoremap("<C-S>", "<cmd>if expand('%') == ''<CR>browse confirm w<CR>else<CR>conf
 --" Write buffer and exit with Ctrl+x
 nnoremap("<C-X>", "<cmd>if expand('%') == ''<CR>browse confirm x<CR>else<CR>confirm x<CR>endif<CR>")
 
---" Window Navigation with Ctrl-[hjkl]
-nnoremap("<C-J> <C-W>", "j")
-nnoremap("<C-K> <C-W>", "k")
-nnoremap("<C-H> <C-W>", "h")
-nnoremap("<C-L> <C-W>", "l")
+--" Write buffer and exit with Ctrl+x
+nnoremap("<C-Q>", "<cmd>if expand('%') == ''<CR>browse confirm q<CR>else<CR>confirm q<CR>endif<CR>")
 
+--" Window Navigation with Ctrl-[hjkl]
+nnoremap("<C-J>", "<C-W>j")
+nnoremap("<C-K>", "<C-W>k")
+nnoremap("<C-H>", "<C-W>h")
+nnoremap("<C-L>", "<C-W>l")
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set({ 'n', 't' }, "<C-Tab>", ToggleTerminal)
+
+-- delete word and go to insert mode
 nnoremap("<C-d>", "diwi")
 
+-- terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set({ 'n', 't' }, "<C-t>", ToggleTerminal)
+
+-- folke/trouble.nvim
+nnoremap("<leader>xx", "<cmd>TroubleToggle<cr>", {silent = true, noremap = true})
+
+nnoremap("<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", {silent = true, noremap = true})
+
+nnoremap("<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", {silent = true, noremap = true})
+
+nnoremap("<leader>xl", "<cmd>TroubleToggle loclist<cr>", {silent = true, noremap = true})
+
+nnoremap("<leader>xq", "<cmd>TroubleToggle quickfix<cr>", {silent = true, noremap = true})
+
+nnoremap("gR", "<cmd>TroubleToggle lsp_references<cr>", {silent = true, noremap = true})
+
+-- romgrk/barbar.nvim
+local barbar_opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+nnoremap('<C-[>', '<Cmd>BufferPrevious<CR>', barbar_opts)
+nnoremap('<C-]>', '<Cmd>BufferNext<CR>', barbar_opts)
+-- Re-order to previous/next
+nnoremap('<C-<>', '<Cmd>BufferMovePrevious<CR>', barbar_opts)
+nnoremap('<C->>', '<Cmd>BufferMoveNext<CR>', barbar_opts)
+-- Goto buffer in position...
+nnoremap('<C-1>', '<Cmd>BufferGoto 1<CR>', barbar_opts)
+nnoremap('<C-2>', '<Cmd>BufferGoto 2<CR>', barbar_opts)
+nnoremap('<C-3>', '<Cmd>BufferGoto 3<CR>', barbar_opts)
+nnoremap('<C-4>', '<Cmd>BufferGoto 4<CR>', barbar_opts)
+nnoremap('<C-5>', '<Cmd>BufferGoto 5<CR>', barbar_opts)
+nnoremap('<C-6>', '<Cmd>BufferGoto 6<CR>', barbar_opts)
+nnoremap('<C-7>', '<Cmd>BufferGoto 7<CR>', barbar_opts)
+nnoremap('<C-8>', '<Cmd>BufferGoto 8<CR>', barbar_opts)
+nnoremap('<C-9>', '<Cmd>BufferGoto 9<CR>', barbar_opts)
+nnoremap('<C-0>', '<Cmd>BufferLast<CR>', barbar_opts)
+-- Pin/unpin buffer
+nnoremap('<C-A-p>', '<Cmd>BufferPin<CR>', barbar_opts)
+-- Close buffer
+nnoremap('<C-w>', '<Cmd>BufferClose<CR>', barbar_opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+nnoremap('<C-p>', '<Cmd>BufferPick<CR>', barbar_opts)
+-- Sort automatically by...
+nnoremap('<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', barbar_opts)
+nnoremap('<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', barbar_opts)
+nnoremap('<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', barbar_opts)
+nnoremap('<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', barbar_opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
