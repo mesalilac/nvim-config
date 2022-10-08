d164413f6824413684d7c09254e456b1b6a7f27a
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-08 16:17:29 +0300

    lua/

---
+++ lua/user/set.lua
@@ -0,0 +1,36 @@
+o = vim.o
+
+o.tabstop = 4 
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+o.exrc = true
+o.nohlsearch = true
+o.relativenumber = true
+o.nu = true
+o.nowrap = true
+o.guifont = "Iosevka 20"
+o.ignorecase = true
+o.smartcase = true
+o.noswapfile = true
+o.nobackup = true
+o.undodir = "~/.vim/undodir"
+o.undofile = true
+o.incsearch = true
+o.termguicolors = true
+o.scrolloff = 8
+o.noshowmode = true
+o.colorcolumn = 80
+o.signcolumn = "yes"
+o.number = true
+o.noerrorbells = true
+o.title = true
+o.showmatch = true
+o.autoindent = true
+o.mouse = "a"
+o.clipboard = "unnamedplus"
+o.updatetime = 100
+
+vim.cmd("syntax on")
+vim.cmd("filetype plugin on")
