8d4adc1953e23f28f0c0fc4d70486de462d01863
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-10 04:26:00 +0300

    require plugin config

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
 require("user.set")
-require("user.packer")
+require("user.plugins")
 require("user.remap")
+
+-- require plugins
+require("user.plugin_config.barbar")
+-- require("user.plugin_config.coc-nvim")
+require("user.plugin_config.dashboard-nvim")
+require("user.plugin_config.nvim-colorizer")
+require("user.plugin_config.nvim-hardline")
+require("user.plugin_config.nvim-tree")
+require("user.plugin_config.presence-nvim")
+require("user.plugin_config.rainbow")
+require("user.plugin_config.telescope")
+require("user.plugin_config.todo-comments")
+require("user.plugin_config.trouble-nvim")
+require("user.plugin_config.vim-gitgutter")
 
