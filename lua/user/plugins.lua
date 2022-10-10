015a81b4336c083a3cdc531c0e36374b1c6a5043
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-10 04:25:12 +0300

    rename packer.lua to plugins.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,7 +1,44 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -78,10 +115,10 @@ return require('packer').startup(function(use)
     use 'andweeb/presence.nvim'
 
     -- Nodejs extension host for vim & neovim, load extensions like VSCode and host language servers
-    use {
-        'neoclide/coc.nvim',
-        branch = 'release'
-    }
+    -- use {
+    --     'neoclide/coc.nvim',
+    --     branch = 'release'
+    -- }
 
     -- vim-snipmate default snippets (Previously snipmate-snippets)
     use 'honza/vim-snippets'
@@ -91,8 +128,8 @@ return require('packer').startup(function(use)
 
     -- --  A neovim lua plugin to help easily manage multiple terminal windows
     -- use {
-    --     "akinsho/toggleterm.nvim",
-    --     tag = '*'
+    --      "akinsho/toggleterm.nvim",
+    --      tag = '*'
     -- }
 
 -- Colorschemes
@@ -103,6 +140,12 @@ return require('packer').startup(function(use)
     use 'gruvbox-community/gruvbox'
     use 'chriskempson/vim-tomorrow-theme'
     use 'bluz71/vim-moonfly-colors'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
 
 --Plug 'reconquest/vim-pythonx'
