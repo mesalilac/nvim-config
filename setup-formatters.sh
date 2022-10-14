cacf8f565d6d89a63af17dc02fd716769967c924
abdalrzag eisa <bmjfdrh@gmail.com>
2022-10-14 16:27:48 +0300

    add black formatter

--- setup-formatters.sh
+++ setup-formatters.sh
@@ -13,7 +13,8 @@ All formatters that i use in with null-ls.nvim
 
 read -s -n 1 -p "Press any key to continue... "
 
-apowdkopawkdkopawd
-
-echo "installing lua formatter"
+echo "Installing lua formatter"
 cargo install stylua
+
+echo "Installing python formatter"
+pip install black
