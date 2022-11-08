5c19d802dbdb5e2e9ee47732503e6c8ef102c780
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-08 18:58:25 +0300

    change color

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,13 +1,12 @@
--- vim.cmd("color one")
+vim.cmd("color one")
 -- vim.cmd("color vscode")
 -- vim.cmd("color tokyonight-night")
 -- vim.cmd("color doom-one")
 
 -- oh-lucy
-vim.g.oh_lucy_italic_comments = true
-vim.g.oh_lucy_italic_keywords = true
-vim.g.oh_lucy_italic_booleans = true
-vim.g.oh_lucy_italic_function = true
-vim.g.oh_lucy_italic_variables = true
-
-vim.cmd("color oh-lucy")
+-- vim.g.oh_lucy_italic_comments = true
+-- vim.g.oh_lucy_italic_keywords = true
+-- vim.g.oh_lucy_italic_booleans = true
+-- vim.g.oh_lucy_italic_function = true
+-- vim.g.oh_lucy_italic_variables = true
+-- vim.cmd("color oh-lucy")
