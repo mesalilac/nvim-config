b6b957761cc84de9bde860e67e13eeea1e6ac8c3
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-17 23:33:14 +0300

    change color

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,12 +1,12 @@
-vim.cmd("color one")
--- vim.cmd("color vscode")
--- vim.cmd("color tokyonight-night")
--- vim.cmd("color doom-one")
+vim.g.oh_lucy_italic_comments = true
+vim.g.oh_lucy_italic_keywords = true
+vim.g.oh_lucy_italic_booleans = true
+vim.g.oh_lucy_italic_function = true
+vim.g.oh_lucy_italic_variables = true
 
--- oh-lucy
--- vim.g.oh_lucy_italic_comments = true
--- vim.g.oh_lucy_italic_keywords = true
--- vim.g.oh_lucy_italic_booleans = true
--- vim.g.oh_lucy_italic_function = true
--- vim.g.oh_lucy_italic_variables = true
--- vim.cmd("color oh-lucy")
+-- Activates italicized comments (make sure your terminal supports italics)
+vim.g.codedark_italics=1
+-- Make the background transparent
+vim.g.codedark_transparent=1
+
+vim.cmd("color codedark")
