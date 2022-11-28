60b2b9413a0d1d64b6c05564c3234eb4249ec20b
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-29 01:21:51 +0300

    fix indent_blankline config

--- init.lua
+++ init.lua
@@ -2,4 +2,17 @@ require("user.set")
 require("user.plugins")
 require("user.remap")
 
+vim.g.oh_lucy_italic_comments = true
+vim.g.oh_lucy_italic_keywords = true
+vim.g.oh_lucy_italic_booleans = true
+vim.g.oh_lucy_italic_function = true
+vim.g.oh_lucy_italic_variables = true
+
+-- Activates italicized comments (make sure your terminal supports italics)
+vim.g.codedark_italics=1
+-- Make the background transparent
+-- vim.g.codedark_transparent=1
+
+vim.cmd("color darkcode")
+
 require("user.plugin_config")
--- lua/user/plugin_config/indent-blankline.lua
+++ lua/user/plugin_config/indent-blankline.lua
@@ -7,8 +7,14 @@ end
 -- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "eol:↴"
 
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { fg = "#E06C75", nocombine = true })
+vim.api.nvim_set_hl(0, "IndentBlanklineContextStart", { bg = "#2e2d2d", nocombine = true })
+
 indent_blankline.setup({
 	space_char_blankline = " ",
 	show_current_context = true,
 	show_current_context_start = true,
+	char_list_blankline = { "|", "¦", "┆", "┊" },
 })
