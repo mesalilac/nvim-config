45439cb15443bb873c2df62e69bba85a95343857
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-13 03:17:42 +0300

    add default settings

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -180,7 +180,28 @@ return packer.startup(function(use)
 	use({
 		"lewis6991/satellite.nvim",
 		config = function()
-			require("satellite").setup()
+			require("satellite").setup({
+				current_only = false,
+				winblend = 50,
+				zindex = 40,
+				excluded_filetypes = {},
+				width = 2,
+				handlers = {
+					search = {
+						enable = true,
+					},
+					diagnostic = {
+						enable = true,
+					},
+					gitsigns = {
+						enable = true,
+					},
+					marks = {
+						enable = true,
+						show_builtins = false, -- shows the builtin marks like [ ] < >
+					},
+				},
+			})
 		end,
 	})
 
