0004df1ec6dbdcac2285c394c28176ce4ef1d151
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-08 22:18:30 +0300

    fix script and add pylint

--- setup-formatters.sh
+++ setup-formatters.sh
@@ -2,13 +2,13 @@
 
 # setup-formatters
 # this script will download and install
-# all formatters that i use in with null-ls.nvim
+# all formatters that i use in null-ls.nvim
 
 set -e
 
 echo -e "
 This script will download and install
-All formatters that i use in with null-ls.nvim
+All formatters that i use in null-ls.nvim
 "
 
 read -s -n 1 -p "Press any key to continue... "
@@ -17,7 +17,10 @@ echo "Installing lua formatter"
 cargo install stylua
 
 echo "Installing python formatter"
-pip install black
+sudo pacman -S black
+
+echo "Installing python linter"
+sudo pacman -S python-pylint
 
 echo "Installing prettier"
 npm install -g prettier
