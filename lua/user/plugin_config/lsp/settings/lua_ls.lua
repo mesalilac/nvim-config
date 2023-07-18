e0c1260b062f0a04d76069959656ea98e1ef0ea0
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-18 09:10:36 +0300

    format

--- lua/user/plugin_config/lsp/settings/lua_ls.lua
+++ lua/user/plugin_config/lsp/settings/lua_ls.lua
@@ -1,21 +1,21 @@
 return {
-    settings = {
-        Lua = {
-            completion = {
-                enable = true,
-            },
-            diagnostics = {
-                enable = true,
-                globals = {
-                    "vim",
-                },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true,
-                },
-            },
-        },
-        }
+	settings = {
+		Lua = {
+			completion = {
+				enable = true,
+			},
+			diagnostics = {
+				enable = true,
+				globals = {
+					"vim",
+				},
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
 }
