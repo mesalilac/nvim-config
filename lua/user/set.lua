6eb255a9aa53fc5ff4ab34ccb080834cff9240e5
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-18 05:43:07 +0300

    change cursor to block

--- lua/user/set.lua
+++ lua/user/set.lua
@@ -8,9 +8,12 @@ o.shiftwidth = 4
 o.expandtab = true
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 o.conceallevel = 0 -- so that `` is visible in markdown files
+
 -- Enable blinking together with different cursor shapes for insert/command mode, and cursor highlighting:
-o.guicursor =
-	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+-- o.guicursor =
+-- 	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+o.guicursor = "i:block"
+
 o.smartindent = true
 o.exrc = true
 o.relativenumber = true
