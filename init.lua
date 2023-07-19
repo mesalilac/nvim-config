84204de237b5aca5eaa2de7d3adc0d77d77c34ec
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-19 16:51:46 +0300

    fix symbols-outline.nvim colors

--- init.lua
+++ init.lua
@@ -20,3 +20,5 @@ require("user.plugin_config")
 require("user.remap")
 
 vim.cmd("hi TreesitterContextBottom gui=underline guisp=Grey")
+vim.api.nvim_set_hl(0, "FocusedSymbol", { fg = "#111111", bg = "#D7BA7D" })
+vim.api.nvim_set_hl(0, "SymbolsOutlineConnector", { fg = "#5A5A5A", bg = "NONE" })
