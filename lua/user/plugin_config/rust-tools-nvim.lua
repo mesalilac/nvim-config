4cdc3e3fe1a022ee7f7974cb3593a6fcdd4ceba5
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-19 19:34:05 +0300

    run clippy instead of check

--- lua/user/plugin_config/rust-tools-nvim.lua
+++ lua/user/plugin_config/rust-tools-nvim.lua
@@ -162,17 +162,18 @@ local opts = {
 	-- all the opts to send to nvim-lspconfig
 	-- these override the defaults set by rust-tools.nvim
 	-- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
-	-- server = {
-	-- 	-- standalone file support
-	-- 	-- setting it to false may improve startup time
-	-- 	standalone = true,
-	-- 	on_attach = function(_, bufnr)
-	-- 		-- Hover actions
-	-- 		vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-	-- 		-- Code action groups
-	-- 		vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-	-- 	end,
-	-- }, -- rust-analyzer options
+	server = {
+		-- standalone file support
+		-- setting it to false may improve startup time
+		standalone = false,
+		settings = {
+			["rust-analyzer"] = {
+				checkOnSave = {
+					command = "clippy",
+				},
+			},
+		},
+	}, -- rust-analyzer options
 
 	-- debugging stuff
 	dap = {
