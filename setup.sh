baf444f14d91ece055fafd1b3f2f7bff5f147437
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-26 14:37:53 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,29 @@
 #!/bin/env bash
 
-# setup-formatters
 # this script will download and install
 # all formatters that i use in null-ls.nvim
+# and other stuff
 
 set -e
-
-echo -e "
-This script will download and install
-All formatters that i use in null-ls.nvim
-"
-
-read -s -n 1 -p "Press any key to continue... "
 
 echo "Installing lua formatter"
 cargo install stylua
 
 echo "Installing python formatter"
-sudo pacman -S python-black
+sudo apt-get install black
 
 echo "Installing python linter"
-sudo pacman -S python-pylint
+sudo apt-get install pylint
 
 echo "Installing prettier"
-npm install -g prettier
+yarn global add prettier
+
+# Install Node.js provider
+yarn global add neovim
+
+# Install ripgrep for telescope
+cargo install ripgrep
+
+# Install fd for telescope
+cargo install fd-find
+
