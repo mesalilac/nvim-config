895585cd74efd595a5bb2fbb4d3aea71bd983c14
abdalrzag eisa <bmjfdrh@gmail.com>
2023-07-29 10:24:16 +0300

    enable tsserver formater

--- lua/user/plugin_config/lsp/handlers.lua
+++ lua/user/plugin_config/lsp/handlers.lua
@@ -83,9 +83,10 @@ end
 M.on_attach = function(client, bufnr)
 	vim.notify(client.name .. " starting...")
 	-- TODO: refactor this into a method that checks if string in list
-	if client.name == "tsserver" then
-		client.server_capabilities.documentFormattingProvider = false
-	end
+
+	-- if client.name == "tsserver" then
+	-- 	client.server_capabilities.documentFormattingProvider = false
+	-- end
 	-- lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
 end
