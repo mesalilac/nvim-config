451eafebf845f9295c31866cc786320ab363155b
abdalrzag eisa <bmjfdrh@gmail.com>
2023-09-09 11:09:58 +0300

    Remove old code

--- init.lua
+++ init.lua
@@ -3,19 +3,11 @@ require("user.plugins")
 
 -- require("user.darkcode")
 
--- vim.g.oh_lucy_italic_comments = true
--- vim.g.oh_lucy_italic_keywords = true
--- vim.g.oh_lucy_italic_booleans = true
--- vim.g.oh_lucy_italic_function = true
--- vim.g.oh_lucy_italic_variables = true
-
--- Activates italicized comments (make sure your terminal supports italics)
--- vim.g.codedark_italics=1
--- Make the background transparent
--- vim.g.codedark_transparent=1
-
--- vim.cmd("color gruvbox-material")
--- vim.cmd("color catppuccin-macchiato")
+vim.g.oh_lucy_italic_comments = true
+vim.g.oh_lucy_italic_keywords = true
+vim.g.oh_lucy_italic_booleans = true
+vim.g.oh_lucy_italic_function = true
+vim.g.oh_lucy_italic_variables = true
 
 require("user.plugin_config")
 require("user.remap")
--- lua/user/darkcode.lua
+++
@@ -1,6 +0,0 @@
-local darkcode = require("darkcode")
-
-darkcode.setup({
-	italic_comments = true,
-	italic_keywords = false,
-})
