b8e1b757deb127d1ceec80d13c3be1f69024973f
abdalrzag eisa <bmjfdrh@gmail.com>
2023-09-12 16:54:11 +0300

    use onedark_dark

--- init.lua
+++ init.lua
@@ -1,18 +1,10 @@
 require("user.set")
 require("user.plugins")
 
--- require("user.darkcode")
-
-vim.g.oh_lucy_italic_comments = true
-vim.g.oh_lucy_italic_keywords = true
-vim.g.oh_lucy_italic_booleans = true
-vim.g.oh_lucy_italic_function = true
-vim.g.oh_lucy_italic_variables = true
-
 require("user.plugin_config")
 require("user.remap")
 
-vim.cmd("color oh-lucy")
+vim.cmd("color onedark_dark")
 
 vim.cmd("hi TreesitterContextBottom gui=underline guisp=Grey")
 vim.api.nvim_set_hl(0, "FocusedSymbol", { fg = "#111111", bg = "#D7BA7D" })
