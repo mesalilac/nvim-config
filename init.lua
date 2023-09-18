6db7e4ad770d92156c31903078b99e3e6f1ba2d7
abdalrzag eisa <bmjfdrh@gmail.com>
2023-09-18 21:20:44 +0300

    Change colorscheme

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ require("user.plugins")
 require("user.plugin_config")
 require("user.remap")
 
-vim.cmd("color material-deep-ocean")
+vim.cmd("color catppuccin")
 
 vim.cmd("hi TreesitterContextBottom gui=underline guisp=Grey")
 vim.api.nvim_set_hl(0, "FocusedSymbol", { fg = "#111111", bg = "#D7BA7D" })
