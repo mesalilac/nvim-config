423b4f37ddd3e9ea71113a44bd4ba4d32cacd14e
Abdalrzag Eisa <bmjfdrh@gmail.com>
2024-04-15 04:50:20 +0300

    fix: lua_ls settings

--- lua/user/plugin_config/lsp/settings/lua_ls.lua
+++ lua/user/plugin_config/lsp/settings/lua_ls.lua
@@ -8,17 +8,17 @@ return {
 				enable = true,
 				globals = {
 					"vim",
-                    "use",
-                    "awesome",
-                    "client",
-                    "root"
+					"use",
+					"awesome",
+					"client",
+					"root",
 				},
 			},
 			workspace = {
 				library = {
 					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
 					[vim.fn.stdpath("config") .. "/lua"] = true,
-                    "/usr/share/awesome/lib" = true,
+					["/usr/share/awesome/lib"] = true,
 				},
 			},
 		},
