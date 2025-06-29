c7583f6a7639b8bb0a2b15461bdcb6a8a11c3d11
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-06-29 11:40:23 +0300

    fix prettier

--- lua/user/plugin_config/alternate-toggler.lua
+++ lua/user/plugin_config/alternate-toggler.lua
@@ -5,9 +5,26 @@ end
 
 alternate_toggler.setup({
 	alternates = {
+		["true"] = "false",
+		["True"] = "False",
+		["TRUE"] = "FALSE",
 		["yes"] = "no",
+		["Yes"] = "No",
+		["YES"] = "NO",
 		["on"] = "off",
+		["On"] = "Off",
 		["active"] = "inactive",
+		["Active"] = "Inactive",
 		["enabled"] = "disabled",
+		["Enabled"] = "Disabled",
+		["1"] = "0",
+		["<"] = ">",
+		["("] = ")",
+		["["] = "]",
+		["{"] = "}",
+		['"'] = "'",
+		['""'] = "''",
+		["+"] = "-",
+		["==="] = "!==",
 	},
 })
--- lua/user/plugin_config/lsp/null-ls.lua
+++ lua/user/plugin_config/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+    return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,33 +11,37 @@ local diagnostics = null_ls.builtins.diagnostics
 -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		require("none-ls.diagnostics.eslint_d"),
-		diagnostics.stylelint,
-		formatting.prettierd,
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		require("none-ls-shellcheck.diagnostics"),
-		require("none-ls-luacheck.diagnostics.luacheck"),
-		-- formatting.rustfmt,
-		-- formatting.clang_format.with({ extra_args = { "-style={BasedOnStyle: llvm, IndentWidth: 4}" } }),
-		diagnostics.zsh,
-	},
+    debug = false,
+    sources = {
+        require("none-ls.diagnostics.eslint_d"),
+        diagnostics.stylelint,
+        formatting.prettierd.with({
+            env = {
+                PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/linter-config/.prettierrc.json"),
+            },
+        }),
+        formatting.black.with({ extra_args = { "--fast" } }),
+        formatting.stylua,
+        require("none-ls-shellcheck.diagnostics"),
+        require("none-ls-luacheck.diagnostics.luacheck"),
+        -- formatting.rustfmt,
+        -- formatting.clang_format.with({ extra_args = { "-style={BasedOnStyle: llvm, IndentWidth: 4}" } }),
+        diagnostics.zsh,
+    },
 
-	-- format on save
-	-- on_attach = function(client, bufnr)
-	-- 	if client.supports_method("textDocument/formatting") then
-	-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	-- 		vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 			group = augroup,
-	-- 			buffer = bufnr,
-	-- 			callback = function()
-	-- 				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-	-- 				-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-	-- 				vim.lsp.buf.format({ async = false })
-	-- 			end,
-	-- 		})
-	-- 	end
-	-- end,
+    -- format on save
+    -- on_attach = function(client, bufnr)
+    -- 	if client.supports_method("textDocument/formatting") then
+    -- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    -- 		vim.api.nvim_create_autocmd("BufWritePre", {
+    -- 			group = augroup,
+    -- 			buffer = bufnr,
+    -- 			callback = function()
+    -- 				-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+    -- 				-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+    -- 				vim.lsp.buf.format({ async = false })
+    -- 			end,
+    -- 		})
+    -- 	end
+    -- end,
 })
