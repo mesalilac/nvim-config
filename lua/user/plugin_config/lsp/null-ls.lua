b480a181752d2b5daa37f3f3360aab23727a0baa
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-07-19 09:01:34 +0300

    only run on condition

--- lua/user/plugin_config/lsp/null-ls.lua
+++ lua/user/plugin_config/lsp/null-ls.lua
@@ -10,15 +10,54 @@ local diagnostics = null_ls.builtins.diagnostics
 
 -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local function has_biome_config(utils)
+    return utils.root_has_file({ ".biome.json", ".biome.jsonc" })
+end
+
+local function has_prettirer_config(utils)
+    return utils.root_has_file({
+        ".prettierrc",
+        ".prettierrc.cjs",
+        ".prettierrc.js",
+        ".prettierrc.json",
+        "prettier.config.cjs",
+        "prettier.config.js",
+        "prettier.config.mjs",
+    })
+end
+
+local function has_eslint_config(utils)
+    return utils.root_has_file({
+        ".eslintrc",
+        ".eslintrc.cjs",
+        ".eslintrc.js",
+        ".eslintrc.json",
+        "eslint.config.cjs",
+        "eslint.config.js",
+        "eslint.config.mjs",
+    })
+end
+
+local function eslint_condition(utils)
+    return has_eslint_config(utils) and not has_biome_config(utils)
+end
+
+local function prettirer_condition(utils)
+    return has_prettirer_config(utils) and not has_biome_config(utils) or
+        not has_prettirer_config(utils) and
+        not has_biome_config(utils) -- if no prettier config, no biome config, use prettier default
+end
+
 null_ls.setup({
     debug = false,
     sources = {
-        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.diagnostics.eslint_d").with({ condition = eslint_condition }),
         diagnostics.stylelint,
         formatting.prettierd.with({
             env = {
                 PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/linter-config/.prettierrc.json"),
             },
+            condition = prettirer_condition,
         }),
         formatting.black.with({ extra_args = { "--fast" } }),
         formatting.stylua,
